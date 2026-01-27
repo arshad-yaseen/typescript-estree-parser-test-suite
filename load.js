8229d9761f2954bbba45328e8c27acf1d33ff138
arshad <arshadpyaseen@gmail.com>
2026-01-27 22:19:41 +0530

    remove load script

--- load.js
+++
@@ -1,9 +0,0 @@
-import Bun from 'bun'
-
-const dest = Bun.argv[2] || '.'
-
-const REPO_URL = "https://github.com/arshad-yaseen/typescript-estree-parser-test-suite"
-
-Bun.spawnSync({
-  cmd: ["git", "clone", "--quiet", "--no-progress", "--single-branch", "--depth", "1", REPO_URL, dest]
-})
